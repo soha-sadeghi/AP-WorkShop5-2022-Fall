fda623feb9d7fe26f56f18a71d32626645e1f96e
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-12-30 13:44:19 +0330

    feat : gets two integers and returns their sum.

--- src/com/Aut/AUTMath.java
+++ src/com/Aut/AUTMath.java
@@ -1,4 +1,14 @@
 package com.Aut;
 
 public class AUTMath {
+    /**
+     * gets two integers and returns their sum.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static int sum(int num1, int num2) {
+        return num1+num2;
+    }
 }

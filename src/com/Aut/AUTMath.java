dce38465a961832b4ef7e6ca37eeabac052eb79a
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-12-30 13:45:07 +0330

    feat : gets two integer and returns their subtraction.

--- src/com/Aut/AUTMath.java
+++ src/com/Aut/AUTMath.java
@@ -11,4 +11,16 @@ public class AUTMath {
     public static int sum(int num1, int num2) {
         return num1+num2;
     }
+
+
+    /**
+     * gets two integer and returns their subtraction.
+     *
+     * @param num1 the num 1
+     * @param num2 the num 2
+     * @return int
+     */
+    public static int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
 }

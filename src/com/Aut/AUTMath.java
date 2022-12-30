fdc81d9f6c12cab5df30126be667cf4e7f821fca
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-12-30 13:42:58 +0330

    feat : created autmath class

---
+++ src/com/Aut/AUTMath.java
@@ -0,0 +1,4 @@
+package com.Aut;
+
+public class AUTMath {
+}
--- src/com/Aut/Main.java
+++ src/com/Aut/Main.java
@@ -1,7 +1,8 @@
-package com.company;
+package com.Aut;
 
 public class Main {
 
     public static void main(String[] args) {
+
     }
 }
